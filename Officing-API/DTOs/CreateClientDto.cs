4fd3c96cd3d3affec1cdaa7a12e693a5aea60e9b
Filip-Mosz <filip.moszczynski@onet.pl>
2026-06-15 18:00:58 +0200

    required  fields marked in Create dtos

--- Officing-API/DTOs/CreateClientDto.cs
+++ Officing-API/DTOs/CreateClientDto.cs
@@ -1,10 +1,12 @@
 using Officing_API.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Officing_API.DTOs;
 
 public class CreateClientDto
 {
-    public int Id { get; set; }
+    [Required(ErrorMessage = "Login is required")]
     public string Login { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Role is required")]
     public RoleEnum Role { get; set; } = RoleEnum.User;
 }
--- Officing-API/DTOs/CreateWorkspaceDto.cs
+++ Officing-API/DTOs/CreateWorkspaceDto.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Officing_API.DTOs;
 
 public class CreateWorkspaceDto
 {
+    [Required(ErrorMessage = "City is required")]
     public string City { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Street is required")]
     public string Street { get; set; } = string.Empty;
     public string StreetNumber { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Post Code is required")]
     public string PostalCode { get; set; } = string.Empty;
+    [Range(1,10000, ErrorMessage = "Daily Rate have to be between 1 and 10000")]
     public decimal DailyRate { get; set; }
+    
 }
